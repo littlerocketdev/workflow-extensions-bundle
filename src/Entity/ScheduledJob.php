a7d4206f37c0845ee50d4b3c1415a1a888ea5034
Kevin <kevin@littlerocket.nl>
2026-05-04 11:30:13 +0200

    Fix the comment syntax on the 'reschedulable' column for symfony 7.2+

--- src/Entity/ScheduledJob.php
+++ src/Entity/ScheduledJob.php
@@ -32,9 +32,8 @@ class ScheduledJob
      *
      * @var boolean
      *
-     * @ORM\Column(type = "boolean", options={"comment": "Defines whether related job can be rescheduled or not")
      */
-    #[ORM\Column(type: "boolean", options: [], columnDefinition: "COMMENT 'Defines whether related job can be rescheduled or not'")]
+    #[ORM\Column(type: "boolean", options: ["comment" => "Defines whether related job can be rescheduled or not"])]
     private bool $reschedulable = true;
 
     /**
